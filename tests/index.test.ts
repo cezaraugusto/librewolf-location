6e0bb58d787c4132a3e1bca5db7d95d416baa1ae
Cezar Augusto <boss@cezaraugusto.net>
2026-06-17 23:46:15 -0300

    Apply eslint-config-auditor formatting and lint rules

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,7 +3,7 @@ import {expect, test, describe} from 'vitest'
 import librewolfLocation from '../src/index'
 
 describe('librewolf-location module', () => {
-  test('returns string or null', () => {
+  it('returns string or null', () => {
     const res = librewolfLocation()
 
     expect(typeof res === 'string' || res === null).toBe(true)
